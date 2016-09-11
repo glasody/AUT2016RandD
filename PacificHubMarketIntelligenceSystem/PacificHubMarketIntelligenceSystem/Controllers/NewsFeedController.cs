7102bf1a9dd92dc9b6333c297ecf09d2842d6f91
Seong Jho <twn0412@autuni.ac.nz>
2016-09-12 04:58:39 +1200

    Adding Newsfeed is now properly adding relationships to tags.

--- PacificHubMarketIntelligenceSystem/PacificHubMarketIntelligenceSystem/Controllers/NewsFeedController.cs
+++ PacificHubMarketIntelligenceSystem/PacificHubMarketIntelligenceSystem/Controllers/NewsFeedController.cs
@@ -6,9 +6,11 @@ using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Neo4jClient.Cypher;
+using Newtonsoft.Json;
 
 namespace PacificHubMarketIntelligenceSystem.Controllers
 {
@@ -17,64 +19,106 @@ namespace PacificHubMarketIntelligenceSystem.Controllers
     {
         [HttpPost]
         [Route("addFeed")]
-        public IHttpActionResult AddFeed(NewsFeed feed)
+        public IHttpActionResult AddFeed(InputNewsFeed feed)
         {
-            var url = ConfigurationManager.AppSettings["GraphDBUrl"];
-            var user = ConfigurationManager.AppSettings["GraphDBUser"];
-            var password = ConfigurationManager.AppSettings["GraphDBPassword"];
-            var client = new GraphClient(new Uri(url), user, password);
-            client.Connect();
-
-            var addedFeed = client.Cypher
-                .Create("(feed:Feed {feed})")
-                .WithParam("feed", feed)
-                .ExecuteWithoutResultsAsync();
-
-            if (feed.Tags.Count > 0)
+            //http://stackoverflow.com/questions/19534511/how-to-create-a-node-with-neo4jclient-in-neo4j-v2
+            //create newsfeed node
+            var tempFeed = new NewsFeed()
             {
-                foreach (var tag in feed.Tags)
-                {
-                    var t = client.Cypher
-                        .Merge("(keyword:Keyword {value : {tag}})")
-                        .WithParam("tag", tag)
-                        .ExecuteWithoutResultsAsync();
-                    AddRelationship(client, "TAGGED_AS", feed, tag);
-                }
+                Title = feed.Title,
+                Url = feed.Url,
+                Description = feed.Description,
+                Author = feed.Author,
+                PubDate = feed.PubDate
+            };
+
+            try
+            {
+                WebApiConfig.GraphClient.Cypher
+                    .Create("(newsfeed:NewsFeed {tempFeed})")
+                    .WithParams(new { tempFeed })
+                    .ExecuteWithoutResults();
+            }
+            catch (NeoException e)
+            {
+                //exeption here
             }
 
-            return Ok(feed);
+            foreach (var tag in feed.Tags)
+            {
+                try
+                {
+                    WebApiConfig.GraphClient.Cypher
+                        .Create("(t:Tag {Value : {tag}})")
+                        .WithParams(new { tag })
+                        .ExecuteWithoutResults();
+                }catch(NeoException e) { }
+
+                WebApiConfig.GraphClient.Cypher
+                    .Match("(n:NewsFeed)", "(t:Tag)")
+                    .Where((NewsFeed n) => n.Url == feed.Url)
+                    .AndWhere((Tag t) => t.Value == tag)
+                    .CreateUnique("(n)-[:TAGGED_AS]->(t)")
+                    .ExecuteWithoutResults();
+            }
+
+            ////http://stackoverflow.com/questions/34675334/is-there-a-way-to-add-multiple-nodes-with-the-net-neo4j-client
+            ////create tag nodes
+            //if (feed.Tags.Any())
+            //{
+            //    try
+            //    {
+            //        WebApiConfig.GraphClient.Cypher
+            //            .Unwind(feed.Tags, "tag")
+            //            .Create("(t:Tag)")
+            //            .Set("t.Value = tag")
+            //            .ExecuteWithoutResults();
+            //    }
+            //    catch(NeoException e) { }
+            //    //create relationships
+            //}
+
+            return Ok();
         }
 
-        public void AddRelationship(GraphClient client, string relationship, NewsFeed feed, string tag)
-        {
-            var rel = client.Cypher
-                .Match("(newFeed:Feed)", "(keyword:Keyword)")
-                .Where((NewsFeed newFeed) => newFeed.Author.Equals(feed.Author))
-                .AndWhere((Keyword keyword) => keyword.value.Equals(tag))
-                .CreateUnique("(newFeed)-[:{relationship}]->(keyword)")
-                .WithParam("relationship", relationship)
-                .ExecuteWithoutResultsAsync();
-        }
+        //https://dzone.com/articles/neo4j-30-with-a-net-driver-neo4jclient
+        //private void CreateUniqueRelationship(int person1Id, int person2Id, string relType, bool twoWay, GraphClient client)
+        //{
+        //    var query = client.Cypher
+        //        .Match("(p1:Person)", "(p2:Person)")
+        //        .Where((Person p1) => p1.Id == person1Id)
+        //        .AndWhere((Person p2) => p2.Id == person2Id)
+        //        .CreateUnique("(p1)-[:" + relType + "]->(p2)");
+
+        //    if (twoWay)
+        //        query = query.CreateUnique("(p1)<-[:" + relType + "]-(p2)");
+        //
+        //    query.ExecuteWithoutResults();
+        //}
 
         [HttpGet]
         public IHttpActionResult GetAll()
         {
-            var url = ConfigurationManager.AppSettings["GraphDBUrl"];
-            var user = ConfigurationManager.AppSettings["GraphDBUser"];
-            var password = ConfigurationManager.AppSettings["GraphDBPassword"];
-            var client = new GraphClient(new Uri(url), user, password);
-            client.Connect();
+            var query = WebApiConfig.GraphClient.Cypher
+                .Start(new {all = All.Nodes})
+                .Return<object>("all");
+                //.Return((news, keyword) => new
+                //{
+                //    NewsFeed = news.As<NewsFeed>(),
+                //    Keyword = keyword.As<Keyword>()
+                //});
 
-            var query = client.Cypher
-                .Start(new { all = All.Nodes})
-                .Return<object>("n");
-
-            return Ok();
+            return Ok(query.Results);
         }
     }
 
-    public class Keyword
+    public class InputNewsFeed
     {
-        public string value { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime PubDate { get; set; }
+        public IEnumerable<string> Tags { get; set; }
     }
 }
